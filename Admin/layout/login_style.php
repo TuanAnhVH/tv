03763f61d1bdf2185dc531e3a2859059f3b3a59f
TuanAnhVH <takaqb@gmail.com>
2020-11-30 11:03:41 +0700

    show_detail

---
+++ Admin/layout/login_style.php
@@ -0,0 +1,34 @@
+<style>
+    .login100-form-logo {
+    font-size: 60px; 
+    color: #333333;
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -moz-box;
+    display: -ms-flexbox;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 120px;
+    height: 120px;
+    border-radius: 50%;
+    background-image: url('../public/Images/admin/bg/logo1.png');
+    background-size: 100% 100%;
+    margin: 0 auto;
+  }
+  .contanner_background
+  {
+    background-image: url('../public/Images/admin/bg/2.jpg');
+    background-size: 100% 100%;
+   
+  }
+  .login100-form-title {
+    font-family: 'Noto Serif', serif;
+    font-size: 40px;
+    color: #fff;
+    line-height: 1.2;
+    text-align: center;
+    display: block;
+  }
+</style>
+
