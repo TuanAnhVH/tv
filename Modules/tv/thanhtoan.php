5b47496e38ae98648352f81423c70c3df8eecb23
TuanAnhVH <takaqb@gmail.com>
2020-12-02 15:06:36 +0700

    add cart

--- Modules/tv/thanhtoan.php
+++ Modules/tv/thanhtoan.php
@@ -1,33 +1,28 @@
 <?php
-
- $nh= new nuochoa();
+ $sp= new sanpham();
  $dh= new donhang();
- $cart= $nh->r->session_get('cart');
+ $cart= $sp->r->session_get('cart');
 ?>
 
-<div id="content">
+<div id="wrap">
  <div>
- <h1>Thông tin khách hàng</h1>
- <!-- <form action="<?php echo $nh->h->get_url('tv/?m=common&a=home&tl=thanhtoantc')?>" id="frmthanhtoan"  method="post"> -->
- <form action="#" id="frmthanhtoan"  method="post">
-        
-            <h3>Tên khách hàng: </h3> 
-            <input name="txttenkhachhang" class="form-control" type="text">
-            <h3>Số điện thoại: </h3> 
-            <input name="txtsodienthoai"  class="form-control" type="text">
-     
-            <h3>Địa chỉ: </h3> 
-            <textarea name="txtdiachi" style="width:80%;height:30%;margin-top:20px;margin-bottom:20px"  class="form-control"></textarea>  
-      
-            <h3>Email: </h3> 
-            <input name="txtemail"  class="form-control" type="email">
-        <br>
-        <input style="display:inline;margin-right:10px;color: white;" class="btnthanhtoan" type="submit" value="Xác nhận">
-        <button class="btnthanhtoan"><a href="<?php echo $nh->h->get_url('tv/?m=common&a=home&tl=giohang&i=gh');?>">Giỏ hàng</a></button>
-        
-  </form>
-  
+  <h1>Thông tin khách hàng</h1>
+  <form action="<?php echo $sp->h->get_url('tv/?m=common&a=home&tl=thanhtoantc')?>" id="frmthanhtoan"  method="post">       
+      <h3>Tên khách hàng: </h3> 
+      <input name="txttenkhachhang" class="form-control" type="text">
+      <h3>Số điện thoại: </h3> 
+      <input name="txtsodienthoai"  class="form-control" type="text">
 
+      <h3>Địa chỉ: </h3> 
+      <textarea name="txtdiachi" style="width:80%;height:30%;margin-top:20px;margin-bottom:20px"  class="form-control"></textarea>  
+
+      <h3>Email: </h3> 
+      <input name="txtemail"  class="form-control" type="email">
+      <br>
+      <input style="display:inline;margin-right:10px;color: white;" class="btnthanhtoan" type="submit" value="Xác nhận">
+      <button class="btnthanhtoan"><a href="<?php echo $sp->h->get_url('tv/?m=common&a=home&tl=giohang&i=gh');?>">Giỏ hàng</a></button>
+          
+  </form> 
  </div>
 </div>
 <script>
@@ -62,7 +57,6 @@ $("#frmthanhtoan").validate({
             },
             
         }
-	
 });
 </script>
 
